db102bc7847693b251553cc37e5dab1802d5246a
reznikovas <ramdirt@mail.ru>
2022-10-09 14:51:04 +0300

    создал модели и миграции

---
+++ database/migrations/2022_10_08_122339_create_objs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objects', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->morphs('objectable');
+            $table->foreignId('parent_id')->nullable()->contrained('objects')->onDelete('cascade');
+            $table->foreignId('team_id')->nullable()->contrained('teams')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('objs');
+    }
+};
